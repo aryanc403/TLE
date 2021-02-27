8c694c86903e195260352cb2ddfb86c6a09cf1cd
Aryan Choudhary <github@aryanc403.com>
2021-02-27 17:18:19 +0530

    Add install packages in script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+install-pkg libcairo2-dev
+install-pkg libgirepository1.0-dev
+install-pkg libpango1.0-dev
+install-pkg pkg-config
+install-pkg python3-dev
+install-pkg gir1.2-pango-1.0
+install-pkg libjpeg-dev
+install-pkg zlib1g-dev
